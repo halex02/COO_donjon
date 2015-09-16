e9c7b63b5cdcebedc4c7ca532feac53986b9f352
halex02 <halex02@msn.com>
2015-09-16 10:54:19 +0200

    premiers fichiers

---
+++ src/dungeon/Room.java
@@ -0,0 +1,14 @@
+package dungeon ;
+
+import character.* ;
+
+public class Room {
+
+    protected List<Character> characters ;
+    protected List<String> exits ;
+
+    public void init () ;
+
+    public
+    
+}

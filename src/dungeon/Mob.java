11be857b95f64ed54f83549ac72b41a9adfe6d08
halex02 <halex02@msn.com>
2015-09-17 19:02:33 +0200

    save

---
+++ src/dungeon/Mob.java
@@ -0,0 +1,25 @@
+public class Mob {
+
+    private String nom;
+    protected Integer pv;
+    protected Integer maxPv;
+    protected Integer strenth;
+    protected Integer toughness;
+    protected Integer acuracy;
+    protected Integer intelligence;
+    protected Integer charisma;
+    private Integer xp;
+    private Integer live;
+    private List<Stuff> stuff;
+    private Weapon weapon;
+    private Armor armor;
+    private Integer po;
+
+    public String getNom(){
+	return nom ;
+    }
+
+    public void setNom(String name) {
+
+	
+}

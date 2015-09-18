9e33a9aefa32b64646b4127eb140dbf52505abd4
Celdin <sylvain.goulliart@gmail.com>
2015-09-18 15:54:48 +0200

    Explo + Combat

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Locale;
+import java.util.Scanner;
 
-import stuff.Stuff;
 import localisation.Local;
 import mob.Mob;
-import dungeons.RoomGenerator;
+import stuff.Stuff;
+import stuff.weapon.impl.WoodenSword;
 import dungeons.Room;
+import dungeons.RoomGenerator;
 
 
 public class Main {
 	private static Room room;
 	private static Mob player;
+	private static Scanner scanner = new Scanner(System.in);
 	
 	private static Integer lootXp(Integer xp){
 		return (xp/10) + 1;
@@ -38,24 +40,84 @@ public class Main {
 					System.out.println(" - " + stuff.getName());
 				}
 				if(taget.getPo() > 0){
-					System.out.println(" - " + taget.getPo() + "PO");
+					System.out.println(" - " + taget.getPo() + " PO");
 				}
-				System.out.println(" - " + lootXp(taget.getXp()) + 1 + "XP");
+				System.out.println(" - " + lootXp(taget.getXp()) + 1 + " XP");
 			}
 		}
 	}
 	
 	private static void action(){
 		Collections.shuffle(room.getMobs());
-		for(Mob mob : room.getMobs()){
-			if(mob.equals(player)){
+		for(int mobIndex = 0; mobIndex < room.getMobs().size(); mobIndex++){
+			if(room.getMobs().get(mobIndex).equals(player)){
 				System.out.println(Local.ACTION_TEXT);
 				System.out.println(Local.ACTION_LIST);
 				if(room.getMobs().size()>1){
 					System.out.println(Local.ACTION_ATTAQUER);
 				}
+				switch (Integer.parseInt(scanner.nextLine())){
+				case 1://Changer de salle
+					System.out.println(Local.ACTION_CHANGER_SALLE);
+					int i = 1;
+					for(String porte : room.getIssues().keySet()){
+						System.out.println((i) + " - " + porte);
+						i = i + 1;
+					}
+					int choix = Integer.parseInt(scanner.nextLine());
+					if(choix > 0 && choix <= room.getMobs().size()){
+						if(room.getMobs().size() > 1){
+							System.out.println(String.format(Local.ACTION_RUN, player.getName()));
+							for(Mob mob2 : room.getMobs()){
+								if(!mob2.equals(player)){
+									attaque(mob2, player);
+								}
+							}
+						}
+						room.getMobs().remove(player);
+						room = room.getIssues().get(room.getIssues().keySet().toArray()[choix - 1]);
+						room.getMobs().add(player);
+						System.out.println(String.format(Local.ACTION_MOVE, player.getName(), room.getDescription()));
+						if(room.getMobs().size() > 1){
+							System.out.println(Local.MOBS_WAITTING);
+						}
+					}else{
+						System.out.println(String.format(Local.IDIOT_ISSUE, player.getName()));
+					}
+					break;
+				case 2://Foullier
+					
+					break;
+				case 3://Iventaire
+					
+					break;
+				case 4://Danser
+					
+					break;
+				case 5://Attaquer
+					if(room.getMobs().size()>1){
+						int i2 = 1;
+						for(Mob mob2 : room.getMobs()){
+							System.out.println((i2) + " - " + mob2.getName());
+							i2 = i2 + 1;
+						}
+						int c = Integer.parseInt(scanner.nextLine());
+						if(c > 0 && c <= room.getMobs().size()){
+							attaque(player, room.getMobs().get(c - 1));
+						}else{
+							System.out.println(String.format(Local.IDIOT_ATTAQUE, player.getName()));
+						}
+					}else{
+						System.out.println(String.format(Local.IDIOT_ATTAQUE, player.getName()));
+					}
+					
+					break;
+
+				default:
+					break;
+				}
 			}else{
-				attaque(mob,player);
+				attaque(room.getMobs().get(mobIndex),player);
 			}
 		}
 	}
@@ -63,11 +125,13 @@ public class Main {
 	public static void main(String[] args) {
 		RoomGenerator generator = new RoomGenerator();
 		room = generator.generate();
-		player = new Mob(null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+		player = new Mob("Dudule", 100, null, null, null, null, null, 0, 1, null, new ArrayList<Stuff>(), new WoodenSword(), null, 0);
+		if(room.getMobs() == null){
+			room.setMobs(new ArrayList<Mob>());
+		}
 		room.getMobs().add(player);
 		do{
 			action();
 		}while(!room.getName().equals(Local.ENDING_ROOM_NAME));
 	}
-
 }
--- src/localisation/Local.java
+++ src/localisation/Local.java
@@ -12,24 +12,24 @@ public final class Local {
 	public final static String ROOM_BOSS_DESCRIPTION = "Il vous attendait, preparez-vous à combatre";
 
 	public final static String ROOM_COULOIR_NAME = "Couloir";
-	public final static String ROOM_COULOIR_DESCRIPTION_1 = "Un long couloir";
-	public final static String ROOM_COULOIR_DESCRIPTION_2 = "Un couloir sombre";
-	public final static String ROOM_COULOIR_DESCRIPTION_3 = "Un couloir glacial";
-	public final static String ROOM_COULOIR_DESCRIPTION_4 = "Un couloir bien decore";
-	public final static String ROOM_COULOIR_DESCRIPTION_5 = "Un couloir lugubre";
+	public final static String ROOM_COULOIR_DESCRIPTION_1 = "un long couloir";
+	public final static String ROOM_COULOIR_DESCRIPTION_2 = "un couloir sombre";
+	public final static String ROOM_COULOIR_DESCRIPTION_3 = "un couloir glacial";
+	public final static String ROOM_COULOIR_DESCRIPTION_4 = "un couloir bien decore";
+	public final static String ROOM_COULOIR_DESCRIPTION_5 = "un couloir lugubre";
 
 	public final static String ROOM_STANDAR_NAME = "Une cellule";
-	public final static String ROOM_STANDAR_DESCRIPTION_1 = "Une cellule";
-	public final static String ROOM_STANDAR_DESCRIPTION_2 = "Une cellule sombre";
-	public final static String ROOM_STANDAR_DESCRIPTION_3 = "Une cellule glacial";
-	public final static String ROOM_STANDAR_DESCRIPTION_4 = "Une cellule bien decore";
-	public final static String ROOM_STANDAR_DESCRIPTION_5 = "Une cellule lugubre";
+	public final static String ROOM_STANDAR_DESCRIPTION_1 = "une cellule";
+	public final static String ROOM_STANDAR_DESCRIPTION_2 = "une cellule sombre";
+	public final static String ROOM_STANDAR_DESCRIPTION_3 = "une cellule glacial";
+	public final static String ROOM_STANDAR_DESCRIPTION_4 = "une cellule bien decore";
+	public final static String ROOM_STANDAR_DESCRIPTION_5 = "une cellule lugubre";
 
 	public final static String[] ISSUE_STANDAR = { "Une porte en bois",
 			"Un tenture", "Une grille", "Une porte" };
 	public final static String ISSUE_PREVIOUS = "En arriere";
 
-	public static final String WEAPON_WOODEN_SWORD_NAME = "epee en bois";
+	public static final String WEAPON_WOODEN_SWORD_NAME = "Epee en bois";
 	public static final String WEAPON_WOODEN_SWORD_EFFECT = "L'epee s\'abbat sur %s dans un \"pof\" minable et inflige %d";
 
 	public final static String MOB_GOBELIN = "Gobelin";
@@ -42,10 +42,16 @@ public final class Local {
 		 		+ "3 - Iventaire\n"
 		 		+ "4 - Danser";
 	 public final static String ACTION_ATTAQUER = "5 - Attaquer\n";
-	 public final static String ACTION_ATTAQUE = "%s attaque avec sa %s";
+	 public final static String ACTION_ATTAQUE = "%s attaque avec \"%s\"";
+	 public final static String ACTION_RUN = "%s tente de s'enfuir";
+	 public final static String ACTION_MOVE = "%s rentre dans %s";
+	 public final static String ACTION_CHANGER_SALLE = "Il y as plusieur issue :";
 
 	 public final static String LAST_PV= "Il lui reste %d pv";
 	 public final static String DEATH= "%s est Mort";
-	 
 	 public final static String LOOT = "Vous obtenez :";
+	 public final static String MOBS_WAITTING = "On vous y attendait !";
+
+	 public final static String IDIOT_ATTAQUE = "%s donne un coup à un enemis que lui seul semble voir";
+	 public final static String IDIOT_ISSUE = "%s se cogne contre le mur";
 }
